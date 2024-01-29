c709f806ee2f383ff1fc8e9edd6e7eea1a6ca5bf
precondition <57645186+precondition@users.noreply.github.com>
2024-01-29 14:45:37 +0100

    Config SVTR tiny for Kuzushiji

---
+++ configs/textrecog/svtr/my_svtr.py
@@ -0,0 +1,30 @@
+_base_ = ['svtr-tiny_20e_st_mj.py']
+
+dictionary = dict(
+        type="Dictionary",
+        dict_file='{{ fileDirName }}/../../../dicts/SKIP_categories.txt',
+        with_padding=True,
+        with_unknown=True,
+)
+
+kuzushiji_textrecog_data_root = "/kaggle/input/bsrk-recognition/resized_train_images/"
+
+kuzushiji_textrecog_train = dict(
+        type="OCRDataset",
+        data_root=kuzushiji_textrecog_data_root,
+        ann_file="../ocr_dataset_resized_train.json",
+        pipeline=None)
+
+train_list = [kuzushiji_textrecog_train]
+
+kuzushiji_textrecog_test = dict(
+        type="OCRDataset",
+        data_root=kuzushiji_textrecog_data_root,
+        ann_file="../ocr_dataset_resized_validation.json",
+        test_mode=True,
+        pipeline=None)
+
+test_list = [kuzushiji_textrecog_test]
+
+val_evaluator = dict(dataset_prefixes=['字'])
+test_evaluator = val_evaluator
