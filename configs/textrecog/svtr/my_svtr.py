203d5e3ecb0ac87635addc4449b7e2eef9c106ee
precondition <57645186+precondition@users.noreply.github.com>
2024-01-29 16:52:37 +0100

    Try confgs/textrecog/ instead

--- configs/textrecog/svtr/my_svtr.py
+++ configs/textrecog/svtr/my_svtr.py
@@ -56,7 +56,7 @@ kuzushiji_textrecog_data_root = ""
 kuzushiji_textrecog_train = dict(
         type="OCRDataset",
         data_root=kuzushiji_textrecog_data_root,
-        ann_file="../ocr_dataset_resized_train.json",
+        ann_file="configs/textrecog/ocr_dataset_resized_train.json",
         data_prefix=dict(img="/kaggle/input/brsk-recognition/resized_train_images/"),
         pipeline=None)
 
@@ -65,7 +65,7 @@ train_list = [kuzushiji_textrecog_train]
 kuzushiji_textrecog_test = dict(
         type="OCRDataset",
         data_root=kuzushiji_textrecog_data_root,
-        ann_file="../ocr_dataset_resized_validation.json",
+        ann_file="configs/textrecog/ocr_dataset_resized_validation.json",
         data_prefix=dict(img="/kaggle/input/brsk-recognition/resized_train_images/"),
         test_mode=True,
         pipeline=None)
