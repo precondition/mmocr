2263a53b868d680cb0b2bace0ed97ea10fbacd7c
precondition <57645186+precondition@users.noreply.github.com>
2024-03-18 17:39:32 +0100

    Set the max_seq_len of encoder and decoder to 1

--- configs/textrecog/svtr/my_svtr.py
+++ configs/textrecog/svtr/my_svtr.py
@@ -10,8 +10,13 @@ model = dict(
         embed_dims=[96, 192, 256],
         depth=[3, 6, 6],
         num_heads=[3, 6, 8],
-        mixer_types=['Local'] * 8 + ['Global'] * 7),
-        max_seq_len=1)
+        mixer_types=['Local'] * 8 + ['Global'] * 7,
+        max_seq_len=1
+        ),
+    decoder=dict(
+        max_seq_len=1
+        )
+    )
 
 
 # svtr-tiny_20e_st_mj.py settings
